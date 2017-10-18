8c436df767b4cd785f3d2543714b6b87e6f7321a
Rishabh <rishabh@hypertrack.io>
2017-10-18 15:02:24 +0530

    Update collection id

---
+++ packages/webtracking-sdk/src/track-collection-id.ts
@@ -0,0 +1,16 @@
+import {HTTrackActions} from "./track-actions";
+
+export class TrackCollectionId {
+  trackActions: HTTrackActions;
+  constructor(private collectionId: string, private pk: string, private options) {}
+
+  public init() {
+    this.trackActions = new HTTrackActions(this.collectionId, 'collectionId', this.pk, this.options);
+    return this.trackActions;
+  }
+}
+
+export function trackCollectionId (lookupId: string, pk: string, options) {
+  let trackCollection = new TrackCollectionId(lookupId, pk, options);
+  return trackCollection.init();
+}
