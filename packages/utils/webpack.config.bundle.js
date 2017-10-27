c7b8acddd660bf31403ffe99a5e2899c72774eb0
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-10-27 16:48:50 +0530

    umd bundle for utils

--- packages/utils/webpack.config.bundle.js
+++ packages/utils/webpack.config.bundle.js
@@ -6,36 +6,41 @@ var WebpackShellPlugin = require('webpack-shell-plugin');
 var path = require('path');
 
 var mainPath = path.resolve(__dirname, 'src', 'ht-utils.ts');
-var nodeConfig = require('./webpack.config.bundle');
 
-var browserSpecConfig = {
+var config = {
+    devtool: 'source-ht-map, inline-source-ht-map',
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.webpack.js', '.web.js', '.ts', '.js', '.png'],
+        alias: {}
+    },
+    entry: mainPath,
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'ht-utils_browser.js',
+        filename: 'ht-utils.js',
         library: "htUtils",
         libraryTarget: "umd"
     },
-    externals: [
-        {
-            'moment-mini': {
-                commonjs: 'moment',
-                commonjs2: 'moment',
-                amd: 'moment',
-                root: 'moment'
-            },
-            'underscore': {
-                commonjs: 'underscore',
-                commonjs2: 'underscore',
-                amd: 'underscore',
-                root: '_'
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    { loader: 'ts-loader' }
+                ]
             }
-        }
+        ]
+    },
+    externals: [
+        'underscore',
+        'moment-mini'
     ],
     plugins: [
         new Webpack.LoaderOptionsPlugin({
             minimize: true,
             debug: false
-        })
+        }),
+        new Webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
         // new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildExit:['cp -r dist ../../../ht-angular/node_modules/ht-js-utils']}),
         // new WebpackShellPlugin({onBuildEnd:['cp -r src ../../../ht-angular/node_modules/ht-js-utils']})
         // new Webpack.IgnorePlugin(/moment-mini$/),
@@ -44,5 +49,4 @@ var browserSpecConfig = {
     ]
 };
 
-var browserConfig = Object.assign({}, nodeConfig, browserSpecConfig);
-module.exports = [nodeConfig, browserConfig];
+module.exports = config;
