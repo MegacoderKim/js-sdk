a81d541bf80baa4870f5e0f4f3339e9dd1c833cf
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-11-27 16:12:12 +0530

    add rx helpers

--- packages/data/src/rx-helpers/data-intercepter.ts
+++ packages/data/src/rx-helpers/data-intercepter.ts
@@ -1,6 +1,9 @@
 import {combineLatest} from "rxjs/observable/combineLatest";
 import {Observable} from "rxjs/Observable";
 import * as _ from "underscore";
+import {Page} from "ht-models";
+import {distinctUntilChanged} from "rxjs/operators";
+import {itemAsPage$} from "./item-as-page";
 
 export const dataWithSelectedId$ = (data$, id$, keys: string[]): Observable<any> => {
   return combineLatest(
@@ -14,6 +17,36 @@ export const dataWithSelectedId$ = (data$, id$, keys: string[]): Observable<any>
             return item.id === id;
           })} : acc;
         }, data) : data;
+    }
+  )
+};
+
+export const listwithSelectedId$ = (list$, id$): Observable<any> => {
+  return combineLatest(
+    list$,
+    id$.pipe(distinctUntilChanged()),
+    (list: Page<any>, id) => {
+      if(!list) return list;
+      return !id ?
+        list :
+        {
+          count: 1,
+          next: null,
+          page: null,
+          results: list.results.filter(item => {
+            return item.id === id;
+          })
+        }
+    }
+  )
+};
+
+export const listWithItem$ = (list$, item$) => {
+  return combineLatest(
+    list$,
+    item$.pipe(itemAsPage$()),
+    (list, itemPage) => {
+      return itemPage ? itemPage : list
     }
   )
 };
