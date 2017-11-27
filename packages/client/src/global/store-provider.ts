4a8c613feeddc3828d1b5dbe6d39034a8c0d5b9a
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-11-27 18:39:28 +0530

    remove store, add apiStoreService

--- packages/client/src/global/store-provider.ts
+++ packages/client/src/global/store-provider.ts
@@ -1,7 +1,28 @@
 import {storeFactory, StoreProvider} from "../store/index";
 import * as fromRoot from "../reducers";
 
-const storeProvider: StoreProvider = storeFactory(fromRoot.reducers);
+// const storeProvider: StoreProvider = storeFactory(fromRoot.reducers);
 
-Object.freeze(storeProvider);
-export const store = storeProvider.STORE_PROVIDERS;
+// Object.freeze(storeProvider);
+// export const store = storeProvider.STORE_PROVIDERS;
+
+export const apiStoreFactory = () => {
+  const storeProvider: StoreProvider = storeFactory(fromRoot.reducers);
+  return storeProvider.STORE_PROVIDERS;
+};
+
+export const ApiStoreService = (() => {
+  var instance: StoreProvider;
+
+  return {
+    getInstance() {
+      if ( !instance ) {
+        instance = apiStoreFactory();
+      }
+      return instance;
+    },
+    getNewInstance() {
+      return apiStoreFactory()
+    }
+  }
+})();
