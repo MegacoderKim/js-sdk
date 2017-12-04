840b1048faf8354e1f2754d81bc7bf0146ca0fc2
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-12-04 14:53:33 +0530

    merge dispatcher, add account dispatcher

--- packages/client/src/dispatchers/user-dispatcher.ts
+++ packages/client/src/dispatchers/user-dispatcher.ts
@@ -3,28 +3,93 @@ import {IUserData} from "ht-models"
 import {IUserAnalytics, IUser, Page, IUserListSummary} from "ht-models";
 import {AllData, ApiType} from "../interfaces";
 
+//placeline
 export const SET_USER_DATA = '[USERS] set user data';
+export const SET_PLACELINE_ID = '[USERS] set placeline id';
+export const TOGGLE_PLACELINE_ID = '[USERS] toggle placeline id';
+export const SET_PLACELINE_QUERY = '[USERS] set placeline query';
+export const ADD_PLACELINE_QUERY = '[USERS] add placeline query';
+export const SET_PLACELINE_LOADING = '[USERS] set placeline loading';
+//list
+export const SET_LIST_ACTIVE = "[USERS] set list active";
+export const SET_LIST_ID = "[USERS] set list id";
+export const SET_LIST_QUERY = "[USERS] set list query";
+export const ADD_LIST_QUERY = "[USERS] add list query";
+export const TOGGLE_LIST_ID = "[USERS] toggle list id";
+//analytics page
 export const SET_USERS_ANALYTICS_PAGE = '[USERS] set user analytics page';
-export const SET_USERS_INDEX_PAGE = '[USERS] set user index page';
-export const SET_USERS_ANALYTICS_ALL = '[USERS] set users analytics all';
-export const SET_USERS_INDEX_ALL = '[USERS] set users index all';
-export const SET_USERS_SUMMARY = '[USERS] set users summary';
-export const SET_USERS_LIST_API_TYPE = '[USERS] set users list api type';
+export const SET_USERS_ANALYTICS_ID = '[USERS] set analytics id';
+export const SET_USERS_ANALYTICS_QUERY = '[USERS] set analytics query';
+export const SET_USERS_ANALYTICS_LOADING = '[USERS] set analytics loading';
 export const SET_USERS_LIST_DATA_MAP = '[USERS] set users list data map';
+
+//analytics all
+export const SET_MARKERS_ACTIVE = "[USERS] set markerS active";
+export const ADD_USERS_ANALYTICS_ALL = '[USERS] add users analytics all';
+export const SET_USERS_ANALYTICS_ALL_QUERY = '[USERS] add analytics all query';
+export const ADD_USERS_ANALYTICS_ALL_QUERY = '[USERS] set analytics all query';
+export const SET_USERS_ANALYTICS_ALL_LOADING = '[USERS] set analytics all loading';
 export const SET_USERS_MARKERS_DATA_MAP = '[USERS] set users listAll data map';
 
-export const INIT_USERS_LIST = "[USERS] init users list";
-export const INIT_USERS_MARKERS = "[USERS] init users listAll";
-export const INIT_USERS = "[USERS] init users";
-export const PAUSE_USERS = "[USERS] pause users";
-
-export const SET_LIST_ACTIVE = "[USERS] set list active";
-export const SET_MARKERS_ACTIVE = "[USERS] set markerS active";
+//summary
 export const SET_SUMMARY_ACTIVE = "[USERS] set summary active";
+export const SET_USERS_SUMMARY = '[USERS] set users summary';
+export const SET_USERS_SUMMARY_LOADING ='[USERS] set summary loading';
+
+export const SET_USERS_INDEX_PAGE = '[USERS] set user index page';
+export const ADD_USERS_INDEX_ALL = '[USERS] add users index all';
+export const SET_USERS_LIST_API_TYPE = '[USERS] set users list api type';
+
 
 export class SetUserData implements Action {
   readonly type = SET_USER_DATA;
   constructor(public payload: IUserData | null) {}
+}
+
+export class SetPlacelineId implements Action {
+  readonly type = SET_PLACELINE_ID;
+  constructor(public payload: string | null) {}
+}
+
+export class TogglePlacelineId implements Action {
+  readonly type = TOGGLE_PLACELINE_ID;
+  constructor(public payload: string) {}
+}
+
+export class SetPlacelineQuery implements Action {
+  readonly type = SET_PLACELINE_QUERY;
+  constructor(public payload: object) {}
+}
+
+export class AddPlacelineQuery implements Action {
+  readonly type = ADD_PLACELINE_QUERY;
+  constructor(public payload: object) {}
+}
+
+export class SetPlacelineLoading implements Action {
+  readonly type = SET_PLACELINE_LOADING;
+  constructor(public payload: boolean|string) {}
+}
+
+
+export class SetUsersListId implements Action {
+  readonly type = SET_LIST_ID;
+  constructor(public payload: string | null) {}
+}
+
+export class ToggleUsersListId implements Action {
+  readonly type = TOGGLE_LIST_ID;
+  constructor(public payload: string) {}
+}
+
+export class SetListQuery implements Action {
+  readonly type = SET_LIST_QUERY;
+  constructor(public payload: object) {}
+}
+
+export class AddListQuery implements Action {
+  readonly type = ADD_LIST_QUERY;
+  constructor(public payload: object) {}
 }
 
 export class SetUsersAnalyticsPage implements Action {
@@ -32,25 +97,32 @@ export class SetUsersAnalyticsPage implements Action {
   constructor(public payload: Page<IUserAnalytics>) {}
 }
 
+
+export class SetUsersAnalyticsLoading implements Action {
+  readonly type = SET_USERS_ANALYTICS_LOADING;
+  constructor(public payload: boolean = true) {}
+}
+
+
 export class SetUsersIndexPage implements Action {
   readonly type = SET_USERS_INDEX_PAGE;
   constructor(public payload: Page<IUser>) {}
 }
 
-export class SetUsersAnalyticsAll implements Action {
-  readonly type = SET_USERS_ANALYTICS_ALL;
+export class SetUsersAnalyticsAll implements Action { //todo change name
+  readonly type = ADD_USERS_ANALYTICS_ALL;
   constructor(public payload: AllData<IUserAnalytics>) {}
 }
 
-export class SetUsersIndexAll implements Action {
-  readonly type = SET_USERS_INDEX_ALL;
+export class SetUsersAnalyticsAllLoading implements Action {
+  readonly type = SET_USERS_ANALYTICS_ALL_LOADING;
+  constructor(public payload: boolean = true) {}
+}
+
+export class SetUsersIndexAll implements Action { //todo change name
+  readonly type = ADD_USERS_INDEX_ALL;
   constructor(public payload: AllData<IUser>) {}
 };
-
-export class SetUsersSummary implements Action {
-  readonly type = SET_USERS_SUMMARY;
-  constructor(public payload: IUserListSummary) {}
-}
 
 export class SetUsersListApiType implements Action {
   readonly type = SET_USERS_LIST_API_TYPE;
@@ -65,26 +137,6 @@ export class SetUsersListDataMap implements Action {
 export class SetUsersMarkersDataMap implements Action {
   readonly type = SET_USERS_MARKERS_DATA_MAP;
   constructor(public payload: (data) => any) {}
-}
-
-export class InitUsersList implements Action {
-  readonly type = INIT_USERS_LIST;
-  constructor(public payload: boolean = true) {}
-}
-
-export class InitUsersMarkers implements Action {
-  readonly type = INIT_USERS_MARKERS;
-  constructor(public payload: boolean = true) {}
-}
-
-export class InitUsers implements Action {
-  readonly type = INIT_USERS;
-  constructor() {}
-}
-
-export class PauseUsers implements Action {
-  readonly type = PAUSE_USERS;
-  constructor() {}
 }
 
 export class SetListActive implements Action {
@@ -102,20 +154,39 @@ export class SetSummaryActive implements Action {
   constructor(public payload: boolean = true) {}
 }
 
+export class SetUsersSummary implements Action {
+  readonly type = SET_USERS_SUMMARY;
+  constructor(public payload: IUserListSummary) {}
+}
+
+export class SetSummaryLoading implements Action {
+  readonly type = SET_USERS_SUMMARY_LOADING;
+  constructor(public payload: boolean = true) {}
+}
+
+
 export type All
   = SetUserData
+  | SetPlacelineId
+  | TogglePlacelineId
+  | SetPlacelineQuery
+  | AddPlacelineQuery
+  | SetPlacelineLoading
+  | SetUsersListId
+  | ToggleUsersListId
+  | SetListQuery
+  | AddListQuery
   | SetUsersAnalyticsPage
+  | SetUsersAnalyticsLoading
   | SetUsersIndexPage
   | SetUsersAnalyticsAll
+  | SetUsersAnalyticsAllLoading
   | SetUsersIndexAll
   | SetUsersSummary
   | SetUsersListApiType
   | SetUsersListDataMap
   | SetUsersMarkersDataMap
-  | InitUsersList
-  | InitUsersMarkers
-  | InitUsers
-  | PauseUsers
   | SetListActive
   | SetMarkersActive
   | SetSummaryActive
+  | SetSummaryLoading
