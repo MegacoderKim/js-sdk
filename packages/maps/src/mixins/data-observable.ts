8c2e48532ffaaf6b13570513c6934b8bac941cf8
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2017-12-05 16:27:55 +0530

    add setPageData$ api, remove setData$

--- packages/maps/src/mixins/data-observable.ts
+++ packages/maps/src/mixins/data-observable.ts
@@ -10,7 +10,7 @@ import {scan} from "rxjs/operators/scan";
 import * as _ from "underscore";
 import {HtPosition} from "ht-data";
 import {combineLatest} from "rxjs/observable/combineLatest";
-import {AllData} from "ht-models";
+import {AllData, Page} from "ht-models";
 
 export interface IMarkersArray {
   valid: any[],
@@ -21,18 +21,17 @@ export function DataObservableMixin <TBase extends Constructor>(Base: TBase) {
   return class extends Base {
     dataSub: Subscription;
     trace: (data, map?) => any;
-    dataSource$: Observable<AllData<any>>;
+    dataSource$: Observable<Page<any>>;
     data$: Observable<IMarkersArray>;
     isValidMapItems?: (data) => boolean;
     getPosition: (data) => HtPosition;
 
     _procData$() {
-      return (source$: Observable<AllData<any>>) => {
+      return (source$: Observable<Page<any>>) => {
         return source$.pipe(
-          map((allData) => {
-            let markers = _.values(allData.resultsEntity);
-            let isNew = allData && allData.count && !allData.next;
-            return _.reduce(markers, (acc, item) => {
+          map((pageData) => {
+            let isNew = pageData && pageData.count && !pageData.next;
+            return _.reduce(pageData.results, (acc, item) => {
               const isValid = this.isValidMapItems ? this.isValidMapItems(item) : !!this.getPosition(item);
               if (isValid) {
                 acc.valid.push(item)
@@ -47,7 +46,7 @@ export function DataObservableMixin <TBase extends Constructor>(Base: TBase) {
       }
     }
 
-    setData$(data$: Observable<AllData<any> | null>, config: SetDataConfig = {}) { //todo take page data, add diff apis
+    setPageData$(data$: Observable<Page<any> | null>, config: SetDataConfig = {}) { //todo take page data, add diff apis
       if (this.dataSub) {
         this.dataSub.unsubscribe();
       }
@@ -55,7 +54,7 @@ export function DataObservableMixin <TBase extends Constructor>(Base: TBase) {
       this.dataSource$ = hide$ ? combineLatest(
         data$,
         hide$.pipe(distinctUntilChanged()),
-        (data, hide) => !!hide ? {resultsEntity: {}, count: 0} : data
+        (data, hide) => !!hide ? {results: [], count: 0, next: '', previous: ''} : data
       ) : data$;
       this.data$ = this.dataSource$.pipe(
         this._procData$()
@@ -79,14 +78,14 @@ export function DataObservableMixin <TBase extends Constructor>(Base: TBase) {
 
       let userData$ = this.data$;
 
-      function isNewId (newItem, old) {
-        if(!old && newItem) return true;
-        if(newItem && old) return  newItem.id !== old.id
-      }
-      function isNewList(newList, old) {
-        if(!old && newList) return true;
-        if(newList && old) return !newList.next && newList.count
-      }
+      // function isNewId (newItem, old) {
+      //   if(!old && newItem) return true;
+      //   if(newItem && old) return  newItem.id !== old.id
+      // }
+      // function isNewList(newList, old) {
+      //   if(!old && newList) return true;
+      //   if(newList && old) return !newList.next && newList.count
+      // }
       let sub = userData$.subscribe(({valid, invalid, isNew}) => {
         this.trace(valid);
         if(isNew) MapService.resetBounds()
