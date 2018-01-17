0f0393a8fc2ec9cb536f79e00dbaecc5425e501b
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-01-17 17:22:27 +0530

    analutics fixes

--- packages/ht-angular/esm2015/ht-angular.js
+++ packages/ht-angular/esm2015/ht-angular.js
@@ -11989,7 +11989,7 @@ AnalyticsContainerComponent.decorators = [
 
 .toolbar {
   width: 100%;
-  padding: 4px 10px;
+  padding: 16px 40px;
   margin-bottom: 20px;
   background: white;
   border-bottom: 1px solid #d6d6d6; }
@@ -12695,6 +12695,7 @@ function mapServiceFactory(mapType) {
     if (mapType === void 0) {
         mapType = 'google';
     }
+    console.log("init map");
     return new HtMapService(mapType);
 }
 /**
