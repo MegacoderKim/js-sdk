c51077b81876d92a628cd8c0d56890b77cfc7e0b
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-01-18 17:02:14 +0530

    use global map class

--- packages/maps/src/global/map-service.ts
+++ packages/maps/src/global/map-service.ts
@@ -1,47 +1,61 @@
 import { ReplaySubject } from "rxjs/ReplaySubject";
 import { LeafletUtils } from "../map-utils/leaflet-map-utils";
 import { GoogleMapUtils } from "../map-utils/google-map-utils";
-import { HtBounds, HtMap, HtMapType } from "../map-utils/interfaces";
+import {HtBounds, HtMap, HtMapType, MapUtils} from "../map-utils/interfaces";
 import * as _ from "underscore";
 import {filter} from "rxjs/operators";
 
-export const GlobalMap = {
-  mapUtils: null,
-  map: null,
-  map$: new ReplaySubject(),
-  clusters: [],
-  itemsSet: [],
+export class GlobalMapService {
+  mapUtils: MapUtils = null;
+  map: HtMap | null = null;
+  map$: ReplaySubject<HtMap | null> = new ReplaySubject();
+  clusters = [];
+  itemsSet = [];
+  mapType: HtMapType;
+  leafletSetBoundsOptions = {
+    animate: true,
+    duration: 0.3
+  };
+  googleSetBoundsOptions = {};
+
+  constructor() {
+    this.map$.subscribe(map => {
+      console.log("map", map);
+      this.map = map;
+    })
+  }
   addToItemsSet(item) {
     const i = this.itemsSet.indexOf(item);
     if (i == -1) this.itemsSet.push(item);
-  },
+  }
   setMap(map: HtMap | null) {
     this.map$.next(map);
-  },
-  getMap() {
-    this.map$.take(1).subscribe(map => {
-      return map;
-    });
-  },
+  }
+  // getMap() {
+  //   this.map$.take(1).subscribe(map => {
+  //     return map;
+  //   });
+  // }
   setMapType(mapType: HtMapType) {
+    this.mapType = mapType;
     this.mapUtils = mapType == "leaflet" ? LeafletUtils : GoogleMapUtils;
-  },
+  }
   addCluster(cluster) {
     if (!this.clusters.includes(cluster)) {
       this.clusters.push(cluster);
-      GlobalMap.map$.pipe(
+      this.map$.pipe(
         filter(data => !!data)
       )
-        .subscribe(map => {
-        cluster.cluster = GlobalMap.mapUtils.getMarkerCluster(map);
-      });
+        .subscribe((map: HtMap) => {
+          cluster.cluster = this.mapUtils.getMarkerCluster(map);
+        });
     }
-  },
+  }
   getBounds(bounds, item) {
     return item.extendBounds(bounds);
-  },
+  }
   getItemsSetBounds(items: any[]) {
-    let bounds = GlobalMap.mapUtils.extendBounds();
+    let bounds = this.mapUtils.extendBounds();
     return _.reduce(
       items,
       (bounds, item) => {
@@ -49,7 +63,7 @@ export const GlobalMap = {
       },
       bounds
     );
-  },
+  }
   resetBounds(bounds?: HtBounds, options?, map?) {
     setTimeout(() => {
       let items = this.itemsSet;
@@ -57,13 +71,8 @@ export const GlobalMap = {
       if (bounds && this.mapUtils.isValidBounds(bounds))
         this.setBounds(bounds, options, map);
     }, 10);
-  },
+  }
 
-  leafletSetBoundsOptions: {
-    animate: true,
-    duration: 0.3
-  },
-  googleSetBoundsOptions: {},
   setBounds(bounds: HtBounds, options?, map?) {
     map = map || this.map;
     if (!map) return false;
@@ -71,14 +80,9 @@ export const GlobalMap = {
       options || this.mapType == "leaflet"
         ? this.leafletSetBoundsOptions
         : this.googleSetBoundsOptions;
-    GlobalMap.mapUtils.setBounds(map || this.map, bounds, options);
+    this.mapUtils.setBounds(map || this.map, bounds, options);
   }
-};
+}
 
-GlobalMap.map$.subscribe(map => {
-  GlobalMap.map = map;
-  // GlobalMap.clusters.forEach(clusterEntity => {
-  //   console.log("clust", clusterEntity);
-  //   clusterEntity.cluster = GlobalMap.mapUtils.getMarkerCluster(map);
-  // });
-});
+export const GlobalMap = new GlobalMapService();
+
