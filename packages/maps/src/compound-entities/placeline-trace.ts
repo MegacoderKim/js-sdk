e3a8ad8a111b948a4e984e57d447b606e399d739
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-01-29 17:19:52 +0530

    clean unused function in placeline

--- packages/maps/src/compound-entities/placeline-trace.ts
+++ packages/maps/src/compound-entities/placeline-trace.ts
@@ -27,16 +27,9 @@ export class Placeline {
   segmentsPolylines;
   stopMarkers;
   actionMarkers;
-  // actionsPolylines = new ActionMarkersTrace();
-  // timelineSegment = new TimelineSegment();
   userMarker;
   animPolyline;
-  // replayMarker = stopMarkersTrace();
-  // eventMarkers = stopMarkersTrace();
   allowedEvents = {};
-  // map;
-  // dataSub: Subscription;
-  // data$: Observable<null | IUserData>;
   mapInstance: MapInstance;
   anim = new TimeAwareAnimation();
   constructor(public options: HtSegmentsTraceOptions) {
@@ -50,34 +43,17 @@ export class Placeline {
     this.animPolyline.setTimeAwareAnimation(this.anim)
   }
 
-  updatePathBearing(path, bearing) {
-    let lastPolyline;
-    let lastPolylineItem;
-    let currentUserMarkerItem;
-    let contentString;
-    // console.log("path", path);
-    // this.mapInstance.mapUtils.setPathPositionTimeArray(lastPolylineItem, path);
-    let entity = this.userMarker.getEntity();
-    let polylineEntity = this.animPolyline.getEntity();
-    let position = path[path.length - 1];
-    // console.log(entity.item.getMap());
-    let content = this.userMarker.getDivContent(entity.data);
-    this.mapInstance.mapUtils.setPath(polylineEntity.item, path)
-    this.userMarker.setContent({ item: entity.item, content });
-    this.mapInstance.mapUtils.updatePosition(entity.item, position)
-  }
-
   get map(): HtMap {
     return this.mapInstance.map;
   }
 
   trace(user: IPlacelineMod, map?) {
-
+    const selectedSegment = user ? user.selectedSegment : null;
     this.setHighlightId(user);
     let userSegments = user && user.segments ? user.segments : [];
     let segType = this.getSegmentTypes(userSegments);
     let lastSegment = segType.lastSegment;
-    if (this.stopMarkers) this.stopMarkers.trace(segType.stopSegment);
+    this.traceStops(segType, selectedSegment, lastSegment);
     if (lastSegment) {
       let restTrips = segType.tripSegment.pop();
       var string = this.getTimeAwarePolyline(lastSegment);
@@ -105,11 +81,16 @@ export class Placeline {
     }
     this.userMarker.trace(user);
     this.traceAction(user);
-
-    // this.traceCurrentUser(_.last(userSegments));
-    // this.traceActionPolyline(user, map, this.getCurrentUserPosition());
-    // this.traceEvents(user, ht-map)
   };
+
+  traceStops(segmentType, selectedSegment, lastSegment) {
+    let stops = segmentType.stopSegment;
+    if (selectedSegment) {
+      stops = selectedSegment.type == 'stop' ? [selectedSegment] : [];
+    }
+    this.stopMarkers.trace(stops);
+
+  }
 
   setHighlightId(user) {
     const data = user.highlightedSegment;
@@ -123,123 +104,21 @@ export class Placeline {
     return segment ? segment.time_aware_polyline : null
   }
 
-  highlightAll(toHighlight) {
-    // this.segmentsPolylines.highlight({}, !!toHighlight);
-    // this.stopMarkers.highlight({}, !!toHighlight)
-  }
-
   extendBounds(bounds) {
-    bounds = this.stopMarkers.extendBounds(bounds);
-    bounds = this.segmentsPolylines.extendBounds(bounds);
-    bounds = this.actionMarkers.extendBounds(bounds);
-    // bounds = this.userMarker.extendItemBounds(bounds);
+    // bounds = this.stopMarkers.extendBounds(bounds);
+    // bounds = this.segmentsPolylines.extendBounds(bounds);
+    // bounds = this.actionMarkers.extendBounds(bounds);
+    bounds = this.userMarker.extendItemBounds(bounds);
     // console.log(bounds, "final");
     return bounds;
-  }
-
-  highlightSegmentId(segmentId: string) {
-    // this.segmentsPolylines.highlight(segmentId);
-    // this.stopMarkers.hightlight(segmentId);
-    // if(segmentId.type == 'trip') {
-    //   this.segmentsPolylines.highlight(segment);
-    //   this.stopMarkers.unHighlight()
-    // } else if (segment.type == 'stop') {
-    //   this.stopMarkers.highlight(segment);
-    //   // this.segmentsPolylines.unHighlight();
-    // }
-    // this.selectSegmentEffect(segment)
-  }
-
-  unselectSegment(segment: ISegment) {
-    if (segment.type == "trip") {
-      // this.segmentsPolylines.resetHighlights();
-      // this.stopMarkers.resetHighlights();
-    } else if (segment.type == "stop") {
-      // console.log("stop select");
-      // this.stopMarkers.resetHighlights();
-      // this.stopMarkers.resetHighlights();
-      // this.segmentsPolylines.resetHighlights();
-    }
-    this.unselectSegmentEffect(segment);
-  }
-
-  selectSegmentEffect(segment) {
-    // this.actionMarkers.highlight({})
-  }
-
-  unselectSegmentEffect(segment) {
-    // this.actionMarkers.resetHighlights()
   }
 
   traceAction(user: IUserData) {
     let actions = user && user.actions ? user.actions : [];
     let filteredActions = _.filter(actions, (action: IAction) => {
       return htAction(action).isValidMarker();
-      // return !!((action.expected_place && action.expected_place.location) || (action.completed_place && action.completed_place.location));
     });
     if (this.actionMarkers) this.actionMarkers.trace(filteredActions);
-  }
-
-  traceActionPolyline(user, map, currentPosition) {
-    let actions = user && user.actions ? user.actions : [];
-    let ongoingAction = currentPosition ? actions : [];
-    if (currentPosition) {
-      let polylines = this.getActionPolylineWithId(
-        currentPosition,
-        ongoingAction
-      );
-      // console.log(polylines);
-      // this.actionsPolylines.trace(polylines)
-    } else {
-      // this.actionsPolylines.removeAll()
-    }
-  }
-
-  getActionPolylineWithId(currentPosition, items: IAction[]) {
-    let sortedAction = _.sortBy(items, action => {
-      return action.eta || action.expected_at;
-    });
-    let polylinesWithId = sortedAction.reduce((acc, action: IAction) => {
-      let finalEta = action.eta || action.expected_at;
-      if (finalEta && !action.display.show_summary) {
-        let { lat, lng } = htAction(action).getPositionsObject().position;
-        let actionPosition = [lat, lng];
-        if (acc.length == 0) {
-          return actionPosition
-            ? [
-                ...acc,
-                {
-                  path: [currentPosition, actionPosition],
-                  id: action.id
-                }
-              ]
-            : acc;
-        } else {
-          let pastPoint = _.last(acc).path[1];
-          return actionPosition
-            ? [
-                ...acc,
-                {
-                  path: [pastPoint, actionPosition],
-                  id: action.id
-                }
-              ]
-            : acc;
-        }
-      } else {
-        return acc;
-      }
-    }, []);
-    return polylinesWithId;
-  }
-
-  selectAction(actionId: string) {
-    if (actionId) {
-      // this.actionMarkers.highlight({id: actionId});
-    } else {
-      // this.actionMarkers.resetHighlights()
-    }
-    this.highlightAll(!!actionId);
   }
 
   protected getSegmentTypes(userSegments: ISegment[]) {
@@ -257,46 +136,6 @@ export class Placeline {
       },
       { tripSegment: [], stopSegment: [] }
     );
-  }
-
-
-  //segments replay
-  clearTimeline() {
-    // this.timelineSegment.clearTimeline();
-  }
-
-  updateTimeline(user: IUserData) {
-    // this.timelineSegment.update(user);
-  }
-
-  getCurrentUserPosition() {
-    let entity = this.userMarker.getEntity();
-    let data = entity.data;
-    // return this.userMarker.getPosition(data)
-  }
-
-  focusUserMarker(map, config) {
-    console.log(this.userMarker);
-    console.error("focus user not implimented");
-    this.mapInstance.mapUtils.setFocus(this.userMarker.getEntity(), map, {
-      force: true,
-      zoom: 15,
-      center: true,
-      ...config
-    });
-    // this.userMarker.setFocus(map);
-  }
-
-  setSegmentPlayCallback(cb) {
-    // this.timelineSegment.playSegmentCallback = cb;
-  }
-
-  setReplayHead(head, map) {
-    if (head && head.currentPosition) {
-      // this.replayMarker.setPositionBearing([head.currentPosition[0], head.currentPosition[1]], head.bearing, map);
-    } else {
-      // this.replayMarker.clear()
-    }
   }
 
 }
