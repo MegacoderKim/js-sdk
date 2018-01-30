404fa4cf0059cdcd300025c6057f7e1b8e7130a4
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-01-30 12:31:01 +0530

    Add drop animation in date picker

--- packages/angular-components/src/app/filters/date-range/date-range.component.ts
+++ packages/angular-components/src/app/filters/date-range/date-range.component.ts
@@ -17,12 +17,12 @@ import {animate, style, transition, trigger} from "@angular/animations";
   animations: [
     trigger('calender-appear', [
       transition(":leave", [
-        style({opacity: 1, height: '*'}),
-        animate('200ms ease-in', style({opacity: 0, height: 0}))
+        style({pointerEvents: 'none'}),
+        animate('150ms ease-in', style({opacity: 0, top: "-10px"}))
       ]),
       transition(":enter", [
-        style({opacity: 0, height: 0}),
-        animate('200ms ease-out', style({opacity: 1, height: '*'}))
+        style({opacity: 0, height: 0, top: "-10px"}),
+        animate('150ms ease-out')
       ]),
       // transition(":enter", [
       //   style({transform: "translateX(-400px)"}),
