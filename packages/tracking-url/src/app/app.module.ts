1ba496a19365a6bb0ee29e92355b85f4d4522417
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-03 15:25:52 +0530

    app shell

--- packages/tracking-url/src/app/app.module.ts
+++ packages/tracking-url/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule} from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
