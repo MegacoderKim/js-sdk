8e18dfd399c27f8fa93391f68cc37ed6f1228af8
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-03 15:26:09 +0530

    app shell add file

---
+++ packages/tracking-url/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {environment} from "../environments/environment";
+
+const routes: Routes = [
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
