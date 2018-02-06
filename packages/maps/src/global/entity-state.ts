63573556c06f799851d7338ac8a63230f8676488
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-06 12:37:44 +0530

    added date entity

--- packages/maps/src/global/entity-state.ts
+++ packages/maps/src/global/entity-state.ts
@@ -2,6 +2,8 @@ import {ReplaySubject} from "rxjs/ReplaySubject";
 import {Observable} from "rxjs/Observable";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
 import {distinctUntilChanged, pluck} from "rxjs/operators";
+import { map } from "rxjs/operators/map";
+import { withLatestFrom } from "rxjs/operators/withLatestFrom";
 
 export class EntityState<S = any> {
   stateSubject$: BehaviorSubject<object> = new BehaviorSubject<object>({});
@@ -10,10 +12,30 @@ export class EntityState<S = any> {
   dataSubject$: ReplaySubject<S[]> = new ReplaySubject<S[]>();
   data$ = this.dataSubject$.asObservable();
 
+  stateTrasfrom: {
+    selector: string,
+    map(data, state): any;
+  }[] = [];
+
+  constructor() {
+    this.data$.pipe(
+      withLatestFrom(this.state$),
+      map(([data, state]) => {
+        return this.stateTrasfrom.reduce((acc, trasform) => {
+          return {...acc, [trasform.selector]: trasform.map(data, acc)}
+        }, state)
+      })
+    ).subscribe(this.stateSubject$)
+  }
   setState(selector, value) {
     const state = {...this.stateSubject$.getValue(), [selector]: value};
     this.stateSubject$.next(state)
   };
+
+  addState(selector, map) {
+    //todo dont add of contains
+    this.stateTrasfrom.push({selector, map})
+  }
 
   selector(selector): Observable<any> {
     return this.state$.pipe(
@@ -25,4 +47,6 @@ export class EntityState<S = any> {
   setData(data: S[]) {
     this.dataSubject$.next(data)
   }
+
+
 }
