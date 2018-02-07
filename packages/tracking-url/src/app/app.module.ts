46412dcec72b4ee8b898b5c5cfdc6f9817d8943d
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-07 18:03:04 +0530

    chore[tracking-url]: add upload script, .env

--- packages/tracking-url/src/app/app.module.ts
+++ packages/tracking-url/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule} from './app-routing.module';
 import { RouterModule } from '@angular/router';
-
-import * as Hammer from 'hammerjs';
-import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-export class MyHammerConfig extends HammerGestureConfig  {
-  overrides = <any>{
-      // override hammerjs default configuration
-      'swipe': { direction: Hammer.DIRECTION_ALL  }
-  }
-}
+
+// import * as Hammer from 'hammerjs';
+// import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+// export class MyHammerConfig extends HammerGestureConfig  {
+//   overrides = <any>{
+//     // override hammerjs default configuration
+//     'swipe': { direction: Hammer.DIRECTION_ALL  }
+//   }
+// }
 
 @NgModule({
   declarations: [
@@ -26,10 +26,10 @@ export class MyHammerConfig extends HammerGestureConfig  {
     AppRoutingModule,
     RouterModule
   ],
-  providers: [ {
-    provide: HAMMER_GESTURE_CONFIG,
-    useClass: MyHammerConfig
-  }],
+  // providers: [ {
+  //   provide: HAMMER_GESTURE_CONFIG,
+  //   useClass: MyHammerConfig
+  // }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
