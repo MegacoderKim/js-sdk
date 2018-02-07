77273d70447859567c8a76aee554137aac1779d8
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-07 13:44:45 +0530

    feat[tracking-url]: add action user in actions trace

--- packages/maps/src/compound-entities/action-trace.ts
+++ packages/maps/src/compound-entities/action-trace.ts
@@ -7,26 +7,32 @@ import { Subscription} from "rxjs/Subscription"
 import { ActionsPolylineTrace} from "../entities/actions-data-polyline";
 import { StartMarkerTrace } from "../entities/start-marker";
 import {TimeAwareAnimation} from "time-aware-polyline";
+import {map} from "rxjs/operators";
+import {ActionUserTrace} from "../entities/action-user";
 
 export class ActionMap {
   mapInstance: MapInstance;
   destination;
   polyline;
   start;
+  user;
+  pulse;
   anim = new TimeAwareAnimation();
   constructor(mapInstance) {
     this.mapInstance = mapInstance;
     this.destination = new DestinationMarker(mapInstance);
     this.polyline = new ActionsPolylineTrace(mapInstance);
     this.polyline.setTimeAwareAnimation(this.anim);
+    this.user = new ActionUserTrace(mapInstance);
+    // this.user.setTimeAwareAnimation(this.anim);
     this.start = new StartMarkerTrace(mapInstance);
-    // this.start.setTimeAwareAnimation(this.anim);
   }
 
   setData$(data$) {
     this.destination.setData$(data$);
     this.polyline.setData$(data$);
-    this.start.setData$(data$)
+    this.start.setData$(data$);
+    this.user.setData$(data$.pipe(map((actions: IAction[]) => actions.map(action => action.user))))
   }
 
 
