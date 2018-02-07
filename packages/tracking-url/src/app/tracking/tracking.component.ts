a80134334b6ae6c6a928ec36130f569cc9671a9a
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-07 19:29:06 +0530

    feat[tracking-url]: add styling to popup and map entities

--- packages/tracking-url/src/app/tracking/tracking.component.ts
+++ packages/tracking-url/src/app/tracking/tracking.component.ts
@@ -7,6 +7,7 @@ import {debounceTime, filter, map, take} from 'rxjs/operators';
 import {ComponentPortal} from "@angular/cdk/portal";
 import {PopperContent} from "../popper/popper-content";
 import Popper from 'popper.js';
+import {Color} from "ht-utility";
 
 @Component({
   selector: 'app-tracking',
@@ -38,7 +39,7 @@ export class TrackingComponent implements OnInit, AfterContentInit {
     const mapInstance = this.mapService.mapInstance;
     this.actionsTrace = new ActionTrace(mapInstance);
     this.actionsTrace.setData$(this.actionsData$);
-
+    this.setStyle()
     this.popups$ = this.actionsData$.pipe(
       map((data) => {
         const entities = this.actionsTrace.destination.entities;
@@ -95,6 +96,84 @@ export class TrackingComponent implements OnInit, AfterContentInit {
 
   trackId(a, b) {
     return b.id
+  }
+
+  setStyle() {
+    const destination = this.actionsTrace.destination;
+    const polyline = this.actionsTrace.polyline;
+    const start = this.actionsTrace.start;
+    const user = this.actionsTrace.user;
+
+    destination.styleObj = {
+      default: {
+        opacity: 1,
+        fillColor: '#00C94B',
+        color: '#00C94B',
+        fill: true,
+        fillOpacity: 1,
+        fillRule: 'nonzero',
+        weight: 5,
+        pane: 'markerPane'
+        // iconAnchor: [12, 12]
+        // iconSize: [35, 35],
+        // className: 'current-action-marker',
+        // iconAnchor: point(15, 43)
+        // iconAnchor: [15, 43]
+      },
+      popup: {
+        // offset: point(0, -35),
+        offset: [0, -5],
+        closeButton: false
+      }
+    }
+
+    polyline.styleObj = {
+      default: {
+        fillColor: '#9013FE',
+        color: '#9013FE',
+        weight: 5
+        // iconAnchor: [12, 12]
+        // iconSize: [35, 35],
+        // className: 'current-action-marker',
+        // iconAnchor: point(15, 43)
+        // iconAnchor: [15, 43]
+      },
+      popup: {
+        // offset: point(0, -35),
+        offset: [0, -5],
+        closeButton: false
+      }
+    }
+
+    start.styleObj = {
+      default: {
+        stroke: false,
+        fillColor: '#9013FE',
+        fill: true,
+        fillOpacity: 1,
+        weight: 3,
+        radius: 7
+      },
+      popup: {
+        // offset: point(0, -35),
+        offset: [0, -5],
+        closeButton: false
+      }
+    }
+
+    user.getDivContent = (data) => {
+      const content = `
+    <div style="border-radius: 50%; height: 60px; width: 60px; background: rgba(144,19,254,0.42) ">
+  <div style="height: 30px; width: 30px; background-image: url('${data.photo}'); background-repeat: no-repeat;
+  background-size: cover;     top: 15px;
+    position: relative;
+    left: 15px;
+    border-radius: 50%;"></div>
+</div> 
+    `;
+      return content
+    }
+
   }
 
 }
