cb2aa960570c57dca3fb956fe5e63e069e51ab34
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-09 16:34:59 +0530

    fix[ht-maps]: fix reset map

--- packages/tracking-url/src/app/core/map-service.ts
+++ packages/tracking-url/src/app/core/map-service.ts
@@ -1,7 +1,37 @@
 import { Injectable, InjectionToken } from '@angular/core';
-import { HtMapClass } from 'ht-maps';
+import { HtMapClass, MapInstance } from 'ht-maps';
+import {Subscription} from "rxjs/Subscription";
+import {fromEventPattern} from "rxjs/observable/fromEventPattern";
 
 export class MapService extends HtMapClass {
+  checkDirtySub: Subscription;
+  mapInstance: MapInstance;
+  mapDirty: boolean = false;
+  actionCompleted: boolean = false;
+  constructor(...args: any[]) {
+    super(...args);
+  }
+  resetCleanMap(action) {
+    if(!this.mapDirty) {
+      this.resetBounds()
+    }
+    if(!this.checkDirtySub) {
+      this.checkDirtySub = this.mapInstance.onEvent$('click mousedown dragstart')
+        .subscribe(data => {
+          this.mapDirty = true;
+      })
+    }
+  }
+
+  resetMap() {
+    if(!this.actionCompleted) this.mapDirty = false;
+  }
+
+  onComplete(action) {
+    this.resetBounds();
+    this.mapDirty = true;
+    this.actionCompleted = true;
+  }
 
 };
 
