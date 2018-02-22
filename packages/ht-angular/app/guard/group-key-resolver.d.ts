9a3d47d47521385fc6e53dc7e32109963fdbb419
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-22 22:55:46 +0530

    chore[build]: remove ht-angular dist

--- packages/ht-angular/app/guard/group-key-resolver.d.ts
+++
@@ -1,8 +0,0 @@
-import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
-import { Observable } from "rxjs/Observable";
-import { HtGroupsService } from "../ht/ht-groups.service";
-export declare class GroupKeyResolver implements Resolve<any> {
-    private groupService;
-    constructor(groupService: HtGroupsService);
-    resolve(next: ActivatedRouteSnapshot): Observable<any>;
-}
--- packages/ht-angular/app/shared/battery-icon/battery-icon.component.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class BatteryIconComponent implements OnInit {
-    battery: number;
-    layout: 'column' | 'row';
-    constructor();
-    ngOnInit(): void;
-    batteryClass(level: number): string;
-}
