122c3f9af69847754dd210add8061ad39d5ec33f
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-22 22:55:19 +0530

    chore[build]: use build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
+# check diff and bump package versions
 lerna publish --skip-git --skip-npm
+
+
+lerna run lib --scope=ht-angular
+
+lerna run copy:styles --scope=ht-angular
+rm -rf packages/angular-components/.ng_pkg_build
+
+# npm publish ./packages/angular-components/dist
+# npm publish ./packages/api
+# npm publish ./packages/client
+# npm publish ./packages/data
+# npm publish ./packages/map
+# npm publish ./packages/models
+
