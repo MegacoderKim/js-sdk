fc5d6f227997153e3eb4a134e737ecd22d1bd97f
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-23 09:56:00 +0530

    build sh updated

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-# check diff and bump package versions
-lerna publish --skip-git --skip-npm
+# build step
+# lerna run lib --scope=ht-angular
 
-
-lerna run lib --scope=ht-angular
+# transform package of ng-components
+# node tools/ng-component.js
 
 lerna run copy:styles --scope=ht-angular
-rm -rf packages/angular-components/.ng_pkg_build
+# rm -rf packages/angular-components/.ng_pkg_build
+
+# check diff and bump package versions
+# lerna publish --skip-git --skip-npm
 
 # npm publish ./packages/angular-components/dist
+
 # npm publish ./packages/api
 # npm publish ./packages/client
 # npm publish ./packages/data
 # npm publish ./packages/map
 # npm publish ./packages/models
+
 
