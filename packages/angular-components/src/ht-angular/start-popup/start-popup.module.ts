6beb2c1f3791cd909d7ba1fdd6e5d190f35610a6
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-02-28 16:50:51 +0530

    refactor[ht-angular]: remove popper module from popup components

--- packages/angular-components/src/ht-angular/start-popup/start-popup.module.ts
+++ packages/angular-components/src/ht-angular/start-popup/start-popup.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StartPopupComponent } from './start-popup.component';
 import {SharedModule} from "../shared/shared.module";
-import {PopperModule} from "../popper/popper.module";
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    PopperModule
   ],
   declarations: [StartPopupComponent],
   exports: [StartPopupComponent]
--- packages/angular-components/src/ht-angular/user-popup/user-popup.module.ts
+++ packages/angular-components/src/ht-angular/user-popup/user-popup.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserPopupComponent } from './user-popup.component';
-import {PopperModule} from "../popper/popper.module";
 import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
-    PopperModule,
     SharedModule
   ],
   declarations: [UserPopupComponent],
