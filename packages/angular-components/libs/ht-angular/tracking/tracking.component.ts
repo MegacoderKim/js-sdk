47ff9eadfcd696da2c9e46a33c220debd0945191
Gaurav Mukherjee <gaurav.mukherjee3@gmail.com>
2018-03-03 20:20:42 +0530

    refactor[ht-angular]: move map entities to trackingMapService

--- packages/angular-components/libs/ht-angular/tracking/tracking.component.ts
+++ packages/angular-components/libs/ht-angular/tracking/tracking.component.ts
@@ -24,9 +24,6 @@ export class TrackingComponent implements OnInit, AfterContentInit {
   @Input() shortCode;
   init: boolean = false;
   actionsData$: Observable<IAction[]>;
-  actionsTrace;
-  destination;
-  polyline;
   popups$;
   userPopup$;
   startPopup$;
@@ -45,15 +42,12 @@ export class TrackingComponent implements OnInit, AfterContentInit {
     // console.log(this.actionSummaryComponent);
     this.actionsData$ = this.trackinService.actions$.asObservable();
     this.init = true;
-    const mapInstance = this.mapService.mapInstance;
-    this.actionsTrace = new ActionTrace(mapInstance, {hasPulse: true});
-    this.actionsTrace.setData$(this.actionsData$);
-    this.setStyle();
+    this.trackingMapService.setData$(this.actionsData$);
     this.popups$ = this.actionsData$.pipe(
       map((data) => {
-        const entities = this.actionsTrace.destination.entities;
+        const entities = this.trackingMapService.actionsTrace.destination.entities;
         const keys = Object.keys(entities);
-        const mapUtils = this.actionsTrace.mapInstance.mapUtils;
+        const mapUtils = this.trackingMapService.actionsTrace.mapInstance.mapUtils;
         return keys.reduce((acc, key) => {
           const entity = entities[key];
           const elem = mapUtils.getElement(entity.item);
@@ -65,14 +59,14 @@ export class TrackingComponent implements OnInit, AfterContentInit {
     this.userPopup$ = this.actionsData$.pipe(
       debounceTime(100), //todo fix this null elem on first render
       map((data) => {
-        const entities = this.actionsTrace.user.entities;
+        const entities = this.trackingMapService.actionsTrace.user.entities;
         const keys = Object.keys(entities);
-        const mapUtils = this.actionsTrace.mapInstance.mapUtils;
+        const mapUtils = this.trackingMapService.mapInstance.mapUtils;
         return keys.reduce((acc, key) => {
           const entity = entities[key];
           const elem = mapUtils.getElement(entity.item);
           // console.log("elem", elem);
-          const onUpdate = this.actionsTrace.user.onEntityUpdate(key);
+          const onUpdate = this.trackingMapService.actionsTrace.user.onEntityUpdate(key);
           return elem ? [...acc, {data: entity.data, elem, id: key, onUpdate}] : acc
         }, [])
       })
@@ -95,16 +89,14 @@ export class TrackingComponent implements OnInit, AfterContentInit {
 
     completedAction$.subscribe((action) => {
       this.loading = false;
-      this.actionsTrace.polyline.toIncludeInBounds = true;
-      this.actionsTrace.start.toIncludeInBounds = true;
       this.trackingMapService.onComplete(action);
     });
 
     this.startPopup$ = completedAction$.pipe(
       map((data: IAction[]) => {
-        const entities = this.actionsTrace.start.entities;
+        const entities = this.trackingMapService.actionsTrace.start.entities;
         const keys = Object.keys(entities);
-        const mapUtils = this.actionsTrace.mapInstance.mapUtils;
+        const mapUtils = this.trackingMapService.actionsTrace.mapInstance.mapUtils;
         return keys.reduce((acc, key) => {
           const entity = entities[key];
           const elem = mapUtils.getElement(entity.item);
@@ -124,107 +116,6 @@ export class TrackingComponent implements OnInit, AfterContentInit {
     return b.id
   }
 
-  setStyle() {
-    const destination = this.actionsTrace.destination;
-    const polyline = this.actionsTrace.polyline;
-    const start = this.actionsTrace.start;
-    const user = this.actionsTrace.user;
-    const pulse = this.actionsTrace.pulse;
-    polyline.toIncludeInBounds = false;
-    start.toIncludeInBounds = false;
-    user.styleObj = {
-      default: {
-        zIndexOffset: 12,
-        iconSize: [24, 24],
-        className: "user-marker"
-      }
-    };
-    pulse.styleObj = {
-      default: {
-        zIndexOffset: 10,
-        iconSize: [24, 24],
-        className: "user-marker"
-      }
-    };
 
-    destination.styleObj = {
-      default: {
-        zIndexOffset: 10,
-        iconSize: [32, 32],
-        className: "destination-marker"
-      }
-    }
-    polyline.toIncludeInBounds = false;
-    polyline.styleObj = {
-      default: {
-        fillColor: '#9013FE',
-        color: '#9013FE',
-        weight: 5
-        // iconAnchor: [12, 12]
-        // iconSize: [35, 35],
-        // className: 'current-action-marker',
-        // iconAnchor: point(15, 43)
-        // iconAnchor: [15, 43]
-      },
-      popup: {
-        // offset: point(0, -35),
-        offset: [0, -5],
-        closeButton: false
-      }
-    }
-
-    // start.styleObj = {
-    //   default: {
-    //     stroke: false,
-    //     fillColor: '#9013FE',
-    //     fill: true,
-    //     fillOpacity: 1,
-    //     weight: 3,
-    //     radius: 7
-    //   },
-    //   popup: {
-    //     // offset: point(0, -35),
-    //     offset: [0, -5],
-    //     closeButton: false
-    //   }
-    // }
-
-    pulse.getDivContent = (data) => {
-      const pulse = data.user.availability_status == 'online' ? 'pulse' : '';
-      const content = `
-    <div class="box-24" style="background: rgba(144,19,254, 1)">
-  <div class="box-24 ${pulse}" style="background: rgba(144,19,254, 1); margin: auto">
-</div>
-</div> 
-    `;
-      return content
-    };
-
-    user.getDivContent = (data, bearing) => {
-      return `<div class="box-24" style="position: absolute">
-    <i class="ion-android-navigate" style="margin: auto; 
-    color: white; 
-    font-size: 17px; 
-    transition: all 2s;
-    transform: rotate(${bearing}deg)"></i>
-</div>`
-    }
-
-    destination.getDivContent = (action: IAction) => {
-      if (action.display.show_summary) {
-        return `<div class="box-32" style="background: #00C94B">
-<!--<div class="icon" style="font-size: 1.5rem; color: white; margin: auto">-->
-        <!--<i class="ion-checkmark"></i>-->
-      <!--</div>-->
-</div>`
-      } else {
-        return `<div class="box-32 is-bordered" style="display: flex; border-color: #250D47">
-    <div class="box-16" style="background: #250D47; margin: auto"></div>
-</div>`
-      }
-
-    }
-
-  }
 
 }
