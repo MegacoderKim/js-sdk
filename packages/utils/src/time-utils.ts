fdac58c6727f4518f5a8cbd903c94c8d8a78956d
gmfun <gaurav@hypertrack.io>
2018-03-12 15:01:55 +0530

    feat[ht-utility]: duration humanize

--- packages/utils/src/time-utils.ts
+++ packages/utils/src/time-utils.ts
@@ -16,3 +16,29 @@ export function GetMinute(time: string) {
 export function HasSameMinute(time1: string, time2: string) {
   return isSameMinute(time1, time2)
 }
+
+export function humanizeDuration(second, scale) {
+  if (second) {
+    let min = second / (scale * 60);
+    let minutes = min % 60;
+    let hours = (min - minutes) / 60;
+    let minuteCeiling = Math.ceil(minutes);
+    let duration = minuteCeiling + " min";
+    if (min >= 60 && hours <= 24) {
+      duration = Math.floor(minutes) + " min";
+      duration = hours + " hr " + duration;
+    } else if (hours > 24) {
+      let day = Math.floor(hours / 24);
+      let hour = Math.floor(hours % 24);
+      duration = day + " d";
+      if (hour > 0 && day < 3) {
+        duration = duration + " " + hour + " hr";
+      }
+    }
+    return duration;
+  } else if (second == 0) {
+    return "1 min";
+  } else {
+    return "";
+  }
+}
