c00be89dbcf730b78091db1f1eb65e00490c5e4d
gmfun <gaurav@hypertrack.io>
2018-03-21 11:48:48 +0530

    feat[ht-angular]: activity specific text on user popup

--- packages/angular-components/libs/ht-angular/user-popup/user-popup.component.ts
+++ packages/angular-components/libs/ht-angular/user-popup/user-popup.component.ts
@@ -1,5 +1,6 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit} from '@angular/core';
 import { IAction } from "ht-models";
+import {HMString} from "ht-utility";
 
 @Component({
   selector: 'ht-user-popup',
@@ -7,13 +8,43 @@ import { IAction } from "ht-models";
   styleUrls: ['./user-popup.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UserPopupComponent implements OnInit {
+export class UserPopupComponent implements OnInit, OnChanges {
   @Input() action: IAction;
   opened: boolean = false;
+  activityData = {
+    title: "speed",
+    body: ""
+  };
   constructor() { }
 
   ngOnInit() {
 
+  }
+
+  ngOnChanges() {
+    switch (this.action.activity.type) {
+      case "stop": {
+        const duration = HMString(this.action.activity.duration, 60);
+        this.activityData = {
+          title: "been here",
+          body: "" + duration
+        };
+        break;
+      };
+      case "walk": {
+        this.activityData = {
+          title: "steps covered",
+          body: "" + this.action.activity.step_count
+        };
+        break;
+      };
+      default: {
+        this.activityData = {
+          title: "speed",
+          body: "--"
+        };
+      }
+    }
   }
 
   openCard() {
