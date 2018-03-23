8c1433c5b708eb60aa5ae7b8192b7f125a2e8dd8
gmfun <gaurav@hypertrack.io>
2018-03-23 16:31:00 +0530

    refactor[dashboard]: add dashboard packages

--- packages/angular-components/src/environments/environment.ts
+++ packages/angular-components/src/environments/environment.ts
@@ -5,6 +5,12 @@
 
 export const environment = {
   production: false,
+  isDeployed: false,
   tileUrl: "",
-  mapKey: ""
+  mapKey: "",
+  tokenName: 'ht-token-prod',
+  domain: "hypertrack.com",
+  mapzenKey: 'mapzen-BUdAvHe',
+  mapUrl: "https://api.mapbox.com/styles/v1/devopshypertrack/cix0ofof8002f2pnwhwfzpco4/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZGV2b3BzaHlwZXJ0cmFjayIsImEiOiJjaXZ3Mjh2YnAwMHQ3MnpvdXgxa3ZoNTZwIn0.Vn-Pr8mFaHy3pxQjG29DNA",
+  stripeKey: ""
 };
