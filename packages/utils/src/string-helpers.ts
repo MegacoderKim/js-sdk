696d45e4897387be4726fe146a606c62cc21a2a3
gmfun <gaurav@hypertrack.io>
2018-04-04 15:10:45 +0530

    fix[ht-utility]: can pass null string to propToString

--- packages/utils/src/string-helpers.ts
+++ packages/utils/src/string-helpers.ts
@@ -4,7 +4,9 @@
  * @param separator : to split the prop string
  */
 export const propToString = (prop: string, separator: string = '_'): string => {
+  if (!prop) return prop;
     const words = prop.split(separator);
+    if (!words.length) return prop;
     return words.reduce((string: string, word: string) => {
         return !!string ? `${string} ${word}` : word[0].toUpperCase() + word.slice(1);
     }, "")
