a32074e87de8db26e6f4213bbf98d5145d9e8d2b
drecodeam <drecodeam@gmail.com>
2018-04-10 12:18:38 +0530

    chore[Dashboard] - remove website angular project

--- packages/website-angular/src/app/directives/copy-block.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CopyBlockDirective } from './copy-block.directive';
-
-describe('CopyBlockDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CopyBlockDirective();
-    expect(directive).toBeTruthy();
-  });
-});
