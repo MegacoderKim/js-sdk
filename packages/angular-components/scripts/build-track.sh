9172339b8c87f811ef6a6e0b49cdc03cfc9ec4d5
gmfun <gaurav@hypertrack.io>
2018-04-12 18:27:29 +0530

    chore[ht-tracking]: add deploy url to tracking experience

--- packages/angular-components/scripts/build-track.sh
+++ packages/angular-components/scripts/build-track.sh
@@ -5,7 +5,7 @@
 # rimraf ../data/node_modules
 # rimraf ../utils/node_modules
 # rimraf ../leaflet-wrapper/node_modules
-ng build --prod --env=release --preserve-symlink --app 2 --stats-json
+ng build --prod --env=release --preserve-symlink -d https://d3sy1lkmbczi2q.cloudfront.net --app 2 --stats-json
 # ng build --prod --env=release --preserve-symlink --app 2 --stats-json
 # webpack-bundle-analyzer dist/stats.json
 cp dist/index2.html dist/index.html
