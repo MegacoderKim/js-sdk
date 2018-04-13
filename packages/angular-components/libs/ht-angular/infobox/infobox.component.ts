38172a844e53e8b9fd246006576e73229855c42f
gmfun <gaurav@hypertrack.io>
2018-04-13 11:45:36 +0530

    feat[ht-angular]: wip hide infobox on tap

--- packages/angular-components/libs/ht-angular/infobox/infobox.component.ts
+++ packages/angular-components/libs/ht-angular/infobox/infobox.component.ts
@@ -1,11 +1,28 @@
 import {Component, Input, OnInit, ViewChild, AfterViewInit, AfterContentInit} from '@angular/core';
 import {PopperContent} from "../popper/popper-content";
 import {HtMapService} from "../ht/ht-map.service";
+import {debounceTime, tap, map, switchMap, take, takeUntil, throttle, throttleTime} from "rxjs/operators";
+import {merge} from "rxjs/observable/merge";
+import {animate, style, transition, trigger, state} from '@angular/animations';
+import {empty} from "rxjs/observable/empty";
 
 @Component({
   selector: 'ht-infobox',
   templateUrl: './infobox.component.html',
-  styleUrls: ['./infobox.component.scss']
+  styleUrls: ['./infobox.component.scss'],
+  animations: [
+    // state('hide', style({opacity: 0})),
+    trigger('fade', [
+      state('hide', style({opacity: 0})),
+      transition("show => hide", [
+        animate('100ms ease-out', style({opacity: 0}))
+      ]),
+      transition("hide => show", [
+        style({opacity: 0}),
+        animate('100ms ease-in', style({opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class InfoboxComponent implements OnInit, AfterViewInit, AfterContentInit {
   @ViewChild(PopperContent) popper: PopperContent;
@@ -15,6 +32,7 @@ export class InfoboxComponent implements OnInit, AfterViewInit, AfterContentInit
     id: string,
     onUpdate?: any;
   };
+  idle: boolean = true;
   constructor(private mapService: HtMapService) { }
 
   ngOnInit() {
@@ -23,8 +41,69 @@ export class InfoboxComponent implements OnInit, AfterViewInit, AfterContentInit
 
   ngAfterViewInit() {
     const mapUtils = this.mapService.mapInstance.mapUtils;
-    const map = this.mapService.mapInstance.map;
-    mapUtils.onEvent(map, 'move', () => {
+    const thismap = this.mapService.mapInstance.map;
+    const mapmoveEnd$ = mapUtils.onEvent$(thismap, 'moveend');
+    const mapmoveStart$ = mapUtils.onEvent$(thismap, 'movestart');
+    const moveEnd$ = mapUtils.onEvent$(thismap, 'mouseup mouseout dragend');
+    const move$ = mapUtils.onEvent$(thismap, 'move');
+    const moveStart$ = mapUtils.onEvent$(thismap, 'click mousedown dragstart');
+    const load$ = mapUtils.onEvent$(thismap, 'load');
+    merge(
+      moveStart$.pipe(
+        throttleTime(100),
+        map(() => true)
+      ),
+      moveEnd$.pipe(
+        debounceTime(100),
+        map(() => false)
+      ),
+      load$.pipe(
+        debounceTime(100),
+        map(() => false)
+      )
+    )
+    //   .pipe(
+    //   tap((move) => {
+    //     // console.log("move", move ? "start" : "end");
+    //     if (move) {
+    //       this.idle = false
+    //     } else {
+    //       this.idle = true;
+    //       if (this.popper) this.popper.scheduleUpdate();
+    //     };
+    //   }),
+    //   switchMap((move) => {
+    //     console.log("move", move ? "start" : "end", move);
+    //     if (move) {
+    //       return empty()
+    //     } else {
+    //       return move$
+    //     }
+    //   })
+    // )
+    //   .subscribe((move) => {
+    //     // console.log("move", move ? "start" : "end", move);
+    //     if (move) {
+    //       this.idle = false
+    //     } else {
+    //       this.idle = true;
+    //     }
+    // });
+
+    // move$.pipe(
+    //   switchMap(() => move$),
+    //   takeUntil(mapmoveStart$.pipe(take(1)))
+    // ).subscribe(() => {
+    //   console.log("up");
+    //   this.popper.scheduleUpdate();
+    // })
+
+
+
+    mapUtils.onEvent$(thismap, 'move').pipe(
+      throttleTime(15)
+    )
+      .subscribe(() => {
       this.popper.scheduleUpdate();
     });
     if (this.item.onUpdate) this.item.onUpdate.subscribe((entity) => {
