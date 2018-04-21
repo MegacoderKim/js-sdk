67f1e2941cdc8fa8eb9094d04deaed6e9f0abbab
gmfun <gaurav@hypertrack.io>
2018-04-22 01:19:26 +0530

    refactor[ht-angular]: add view control and htAutoHeight to map container for null view make view responsive

--- packages/angular-components/libs/ht-angular/map-container/map-container.module.ts
+++ packages/angular-components/libs/ht-angular/map-container/map-container.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MapContainerComponent } from './map-container.component';
 import {MapModule} from "../map/map.module";
 import {SharedModule} from "../shared/shared.module";
+import {ListContainerModule} from "../list-container/list-container.module";
 
 @NgModule({
   imports: [
     CommonModule,
+    ListContainerModule,
     MapModule,
     SharedModule
   ],
