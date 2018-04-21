5f31fd2689c61d448a1ab8cdb6cd18dfe6177141
gmfun <gaurav@hypertrack.io>
2018-04-21 14:55:25 +0530

    refactor[ht-angular]: move placeline date module to user filter

--- packages/angular-components/libs/ht-angular/filters/users-filter/users-filter.module.ts
+++ packages/angular-components/libs/ht-angular/filters/users-filter/users-filter.module.ts
@@ -4,13 +4,15 @@ import { UsersFilterComponent } from './users-filter.component';
 import {EntitySearchModule} from "../entity-search/entity-search.module";
 import {SharedModule} from "../../shared/shared.module";
 import {DateRangeModule} from "../date-range/date-range.module";
+import {PlacelineDateModule} from "../../placeline-date/placeline-date.module";
 
 @NgModule({
   imports: [
     CommonModule,
     EntitySearchModule,
     SharedModule,
-    DateRangeModule
+    DateRangeModule,
+    PlacelineDateModule
   ],
   declarations: [UsersFilterComponent],
   exports: [UsersFilterComponent]
