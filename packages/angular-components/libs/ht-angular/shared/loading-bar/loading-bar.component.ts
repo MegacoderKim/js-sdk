7306331e9bfba6f7e70a4907e1b096371df0a2f3
gmfun <gaurav@hypertrack.io>
2018-04-21 14:54:50 +0530

    feat[ht-angular]: loading bar styles as auto

--- packages/angular-components/libs/ht-angular/shared/loading-bar/loading-bar.component.ts
+++ packages/angular-components/libs/ht-angular/shared/loading-bar/loading-bar.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, ElementRef, AfterViewInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'ht-loading-bar',
   templateUrl: './loading-bar.component.html',
   styleUrls: ['./loading-bar.component.less']
 })
-export class LoadingBarComponent implements OnInit {
+export class LoadingBarComponent implements OnInit, AfterViewInit {
+  @HostBinding('style.bottom')
+  bo: string;
+  // setBottom() {
+  //   return this.sanitize.bypassSecurityTrustStyle(`{bottom: ${this.height}px}`)
+  // }
+  height: number = 2;
+  constructor(private sanitize: DomSanitizer, private elem: ElementRef) {
+    this.bo = `${this.height}px`;
+  }
 
-  constructor() { }
+  ngAfterViewInit() {
+    console.log(this.elem.nativeElement.bottom);
+  }
 
   ngOnInit() {
   }
