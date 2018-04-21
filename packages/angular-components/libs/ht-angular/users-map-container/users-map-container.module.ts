a3030a6034ec0e7e33ff5d6705f447b46884b0bf
gmfun <gaurav@hypertrack.io>
2018-04-22 01:34:42 +0530

    chore[ht-angular]: cleanup unused modules

--- packages/angular-components/libs/ht-angular/users-map-container/users-map-container.module.ts
+++ packages/angular-components/libs/ht-angular/users-map-container/users-map-container.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersMapContainerComponent } from './users-map-container.component';
-import {UsersModule} from "../users/users.module";
-import {MapModule} from "../map/map.module";
 import {UsersContainerModule} from "../users-container/users-container.module";
 import {MapContainerModule} from "../map-container/map-container.module";
 import {UsersFilterModule} from "../filters/users-filter/users-filter.module";
 import {SharedModule} from "../shared/shared.module";
-import {ContainerModule} from "../container/container.module";
 
 @NgModule({
   imports: [
     CommonModule,
     UsersContainerModule,
-    MapContainerModule,
     UsersFilterModule,
     SharedModule,
     MapContainerModule,
-    ContainerModule
   ],
   declarations: [UsersMapContainerComponent],
   exports: [UsersMapContainerComponent]
