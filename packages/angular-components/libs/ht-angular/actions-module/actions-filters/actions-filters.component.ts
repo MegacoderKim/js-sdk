4a0aea5071978af73b131281d7722f4450a05ae1
gmfun <gaurav@hypertrack.io>
2018-04-24 14:58:44 +0530

    feat[ht-angular]: wip add actions filter

--- packages/angular-components/libs/ht-angular/actions-module/actions-filters/actions-filters.component.ts
+++ packages/angular-components/libs/ht-angular/actions-module/actions-filters/actions-filters.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {HtActionsService} from "../../ht/ht-actions.service";
+import {HtUsersService} from "../../ht/ht-users.service";
+import {CombineLoadings$} from "ht-data";
 
 @Component({
   selector: 'ht-actions-filters',
@@ -6,10 +9,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./actions-filters.component.scss']
 })
 export class ActionsFiltersComponent implements OnInit {
-
-  constructor() { }
+  @Input() isMobile: boolean;
+  datePickerOptions = {
+    maxDays: 7,
+    customDateRanges: ['today', 'yesterday', 'last_7_days'],
+    isRight: true,
+    hideCalender: this.isMobile,
+  };
+  query$;
+  ordering$;
+  statusFiltes;
+  sortingLabels;
+  loading$;
+  constructor(
+    private actionsService: HtActionsService,
+    private usersService: HtUsersService
+  ) { }
 
   ngOnInit() {
+    this.query$ = this.actionsService.queryLabel$;
+    this.ordering$ = this.actionsService.ordering$;
+    this.statusFiltes = this.actionsService.filters.statusQueryArray;
+    this.sortingLabels = this.actionsService.filters.sortingQueryLabel;
+    this.loading$ = CombineLoadings$(
+      this.actionsService.list.loading$,
+      this.actionsService.summary.loading$,
+      this.usersService.placeline.loading$
+    );
+  }
+
+  onQuery(query) {
+    this.actionsService.list.setQuery(query)
+  }
+
+
+  clearQuery(key) {
+    this.actionsService.list.clearQueryKey(key)
+  }
+
+  setStatus(key, event) {
+    this.onQuery({status: key})
+  }
+
+  closeAction() {
+    this.usersService.placeline.setId(null);
+    // this.actionsService.list.setId(null)
+  }
+
+  setOrdering(key, sign: 0 | 1 = 1) {
+    let ordering = sign ? key : '-'+key;
+    this.onQuery({ordering})
   }
 
 }
--- packages/angular-components/libs/ht-angular/actions-module/actions-filters/actions-filters.module.ts
+++ packages/angular-components/libs/ht-angular/actions-module/actions-filters/actions-filters.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActionsFiltersComponent } from './actions-filters.component';
+import {DateRangeModule} from "../../filters/date-range/date-range.module";
+import {EntitySearchModule} from "../../filters/entity-search/entity-search.module";
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    DateRangeModule,
+    EntitySearchModule,
+    SharedModule
   ],
   declarations: [ActionsFiltersComponent],
   exports: [ActionsFiltersComponent]
