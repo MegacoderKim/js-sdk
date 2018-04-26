ca2edb05114084a73d48b9ded103fe24ecc098a9
gmfun <gaurav@hypertrack.io>
2018-04-26 11:31:34 +0530

    fix[ht-angular]: remove action detail from actions card only actions placeline container has action detail view

--- packages/angular-components/libs/ht-angular/actions-modules/actions-cards-container/actions-cards-container.module.ts
+++ packages/angular-components/libs/ht-angular/actions-modules/actions-cards-container/actions-cards-container.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActionsCardsContainerComponent } from './actions-cards-container.component';
 import {ActionCardModule} from "../action-card/action-card.module";
-import {CollapsableModule} from "../../common/collapsable/collapsable.module";
-import {ActionDetailsModule} from "../action-details/action-details.module";
 
 @NgModule({
   imports: [
     CommonModule,
     ActionCardModule,
-    CollapsableModule,
-    ActionDetailsModule
   ],
   declarations: [ActionsCardsContainerComponent],
   exports: [ActionsCardsContainerComponent]
